b36d34b6b937e89ffa2e9f96279e321bd1a6f043
Tilo Rabs <trabs@web.de>
2022-08-06 16:50:22 +0200

    luxon

--- src/Components/UserInfo.js
+++ src/Components/UserInfo.js
@@ -1,12 +1,14 @@
-import { useEffect, useState } from 'react'
-import Loader from './Loader'
-import { LocationMap } from './LocationMap.js'
+import { useEffect, useState } from 'react';
+import Loader from './Loader';
+import { LocationMap } from './LocationMap.js';
+const { DateTime } = require("luxon");
 
 const UserInfo = () => {
     const [userData, setUserData] = useState({})
     const [userCountryData, setUserCountryData] = useState([])
     const [isDataLoading, setIsDataLoading] = useState(true)
-    const [isCountryDataLoading, setIsCountryDataLoading] = useState(true)
+    const [isCountryDataLoading, setIsCountryDataLoading] = useState(true);
+    let dt = DateTime.now().toLocaleString();
 
     useEffect(() => {
         readIps()
@@ -59,32 +61,42 @@ const UserInfo = () => {
         <>
             <div className="table">
                 <table>
-                    <tr>
-                        <td>{userData.ip}</td>
-                        <td>
-                            <img src={userCountryData[0].flags.png} />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Country</td>
-                        <td>{userCountryData[0].name.common}</td>
-                    </tr>
-                    <tr>
-                        <td>City</td>
-                        <td>{userData.location.city}</td>
-                    </tr>
-                    <tr>
-                        <td>Region</td>
-                        <td>{userData.location.region}</td>
-                    </tr>
-                    <tr>
-                        <td>Country code</td>
-                        <td>{userData.location.country}</td>
-                    </tr>
-                    <tr>
-                        <td>ISP</td>
-                        <td>{userData.isp}</td>
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <td>{userData.ip}</td>
+                            <td>
+                                <img src={userCountryData[0].flags.png} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Country</td>
+                            <td>{userCountryData[0].name.common}</td>
+                        </tr>
+                        <tr>
+                            <td>City</td>
+                            <td>{userData.location.city}</td>
+                        </tr>
+                        <tr>
+                            <td>Region</td>
+                            <td>{userData.location.region}</td>
+                        </tr>
+                        <tr>
+                            <td>Country code</td>
+                            <td>{userData.location.country}</td>
+                        </tr>
+                        <tr>
+                            <td>ISP</td>
+                            <td>{userData.isp}</td>
+                        </tr>
+                        <tr>
+                            <td>Timezone</td>
+                            <td>{userData.location.timezone}</td>
+                        </tr>
+                        <tr>
+                            <td>Local date</td>
+                            <td>{dt}</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
             <LocationMap
